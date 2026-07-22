75111855e2b119e63dcb6d2e33478e696c3fba91
mahmudzahin <mahmudzahin327@gmail.com>
2026-07-22 18:50:59 +0600

    start new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-green-600 p-4 "> 
+      <h2 className="text-4xl text-blue-500 font-bold flex items-center justify-center">
+        This is all about new  <span className="text-red-300 ml-1.5 italic"> Project</span>  
+      </h2>
+    </div>
+  );
+};
+
+export default App;
